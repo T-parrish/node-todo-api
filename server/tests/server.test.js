5017aeae24b622e79cfb04a5473734b983c6eec8
Taylor Parrish <twpearish@gmail.com>
2018-03-15 20:04:54 +0900

    API tests for POST and bad data

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -35,4 +35,22 @@ describe('POST /todos', () => {
 				}).catch((e) => done(e));
 			});
 	});
+	
+	it('should not create todo with invalid body data', (done) => {
+
+		request(app)
+			.post('/todos')
+			.send({})
+			.expect(400)
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				Todo.find().then((todos) => {
+					expect(todos.length).toBe(0);
+					done();
+				}).catch((e) => done(e));
+			});
+	});
 });
