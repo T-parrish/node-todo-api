abd7db74112617186ae8ecd0b44990fb696e40c6
Taylor Parrish <twpearish@gmail.com>
2018-03-18 19:49:04 +0900

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,47 +1,23 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
 
 
-// jwt.io for documentation
-// jwt.sign
-// jwt.verify
-
-var data = {
-	id: 10
-};
-
-// takes the data object and the secret salt
-var token = jwt.sign(data, '123abc');
-
-console.log(token);
-
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded ', decoded);
-
-// var message = 'Look at this. Look at this thing'
-// var hash = SHA256(message).toString();
+var password = '123abc!';
 
 
-// console.log(`Message: ${message}`)
-// console.log(`Hash: ${hash}`)
+// first argument is how many rounds to generate salt, second is call back
+// bigger the number of rounds, the longer this algorithm takes.
 
-// var data = {
-// 	id: 4
-// };
+bcrypt.genSalt(10, (err, salt) => {
+	// thing to hash, salt to use, callback function
+	bcrypt.hash(password, salt, (err, hash) => {
+		console.log(hash);
+	});
+});
 
-// var token = {
-// 	data,
-// 	// salting a hash to make sure that the output cannot be manipulated
-// 	hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// }
+var hashedPassword = '$2a$10$S.E8q0dZBsNSN3C9rBKpxu8H.cXQ5zDrWV9oRe/wH6A6q7WXcigZy'
 
-// token.data.id = 5;
-// token.hash = SHA256(JSON.stringify(token.data)).toString();
-
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-
-// if (resultHash === token.hash) {
-// 	console.log('Data was not changed');
-// } else {
-// 	console.log('Data was changed. Dont trust');
-// }
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res)
+})
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
 
 const _ = require('lodash');
 
@@ -106,6 +107,24 @@ UserSchema.statics.findByToken = function (token) {
 		'tokens.access': 'auth'
 	})
 };
+
+UserSchema.pre('save', function (next) {
+	var user = this;
+
+	// only hashes if the password is changed - stops database from
+	// hashing passwords multiple times and locking people out
+	if (user.isModified('password')) {
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash
+				next();
+			});
+		});
+
+	} else {
+		next();
+	}
+});
 
 var User = mongoose.model('User', UserSchema)
 
